fbb0bf122186462ea5e6c8eafab4da6d8f1dc224
markbayley <markyb152@hotmail.com>
2019-12-25 05:45:04 +1000

    lot link in build

---
+++ master/build/precache-manifest.eda317f08772639e2fcba19ba608350f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a6001cfbeeae59de185d96eeb27253",
+    "url": "./index.html"
+  },
+  {
+    "revision": "955ea096fad49153a6c0",
+    "url": "./static/css/2.d02ea0e6.chunk.css"
+  },
+  {
+    "revision": "a34e133a52862ceeb489",
+    "url": "./static/css/main.07a37c65.chunk.css"
+  },
+  {
+    "revision": "955ea096fad49153a6c0",
+    "url": "./static/js/2.e89cd7e6.chunk.js"
+  },
+  {
+    "revision": "a34e133a52862ceeb489",
+    "url": "./static/js/main.96f944f0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
