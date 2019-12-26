9bf1a6f266a2a3de38905cf611aa0366528ebad2
markbayley <markyb152@hotmail.com>
2019-12-26 11:12:11 +1000

    ..

---
+++ master/build/precache-manifest.77d2c5081112c6bb0574c42ae4ad5169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849b74c11ad7b6a7d38264fa4aff0693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2efd5eb153dab698a32",
+    "url": "/static/css/2.d02ea0e6.chunk.css"
+  },
+  {
+    "revision": "e57d6b58e21cfdd02e37",
+    "url": "/static/css/main.07a37c65.chunk.css"
+  },
+  {
+    "revision": "d2efd5eb153dab698a32",
+    "url": "/static/js/2.1fa93cb2.chunk.js"
+  },
+  {
+    "revision": "e57d6b58e21cfdd02e37",
+    "url": "/static/js/main.a51ec3c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
