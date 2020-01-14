d09a0c239e43448e30ea72d96ce245d0c577d8da
markbayley <markyb152@hotmail.com>
2020-01-14 18:41:07 +1000

    fixed

--- master/src/features/nav/navBar/NavBar.jsx
+++ master/src/features/nav/navBar/NavBar.jsx
@@ -7,76 +7,80 @@ import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from '../../modals/modalActions';
 
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
+});
 
 const actions = {
-    openModal
-}
-
-const mapState = (state) => ({
-    auth: state.firebase.auth,
-    profile: state.firebase.profile
-})
+  openModal,
+};
 
 class NavBar extends Component {
+  handleSignIn = () => {
+    this.props.openModal('LoginModal');
+  };
 
-    handleSignIn = () => {
-        this.props.openModal('LoginModal')
-    };
+  handleRegister = () => {
+    this.props.openModal('RegisterModal');
+  };
 
-    handleRegister = () => {
-        this.props.openModal('RegisterModal')
-    };
+  handleSignOut = () => {
+    this.props.firebase.logout();
+    this.props.history.push('/');
+  };
 
+  render() {
+    const { auth, profile } = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
+    return (
+      <Menu inverted fixed='top'>
+        <Container>
+          <Menu.Item as={NavLink} exact to='/' header>
+            <img src='/assets/logo.png' alt='logo' />
+            Re-vents
+          </Menu.Item>
+          <Menu.Item as={NavLink} exact to='/events' name='Events' />
+          {authenticated && (
+            <Fragment>
+              <Menu.Item as={NavLink} to='/people' name='People' />
+              <Menu.Item as={NavLink} to='/test' name='Test' />
+              <Menu.Item>
+                <Button
+                  as={Link}
+                  to='/createEvent'
+                  floated='right'
+                  positive
+                  inverted
+                  content='Create Event'
+                />
+              </Menu.Item>
+            </Fragment>
+          )}
 
-    handleSignOut = () => {
-        this.props.firebase.logout();
-        this.props.history.push('/');
-    }
-
-    render() {
-        const { auth, profile } = this.props;
-        const authenticated = auth.isLoaded && !auth.isEmpty;
-        return (
-            <Menu inverted fixed="top">
-                <Container >
-                    <Menu.Item as={NavLink} exact to='/' header>
-                        <img src="/assets/logo3.png" alt="logo" />                   
-                     </Menu.Item>
-                    <Menu.Item as={NavLink} exact to='/events' icon="home" style={{marginLeft: -25}}/>
-                    {authenticated && (
-                        <Fragment>
-                            <Menu.Item as={Link} to={`/profile/${auth.uid}`} text="My Profile" icon="user" />
-                            {/*<Menu.Item as={NavLink} to='/test' icon="chat" />*/}
-                            <Menu.Item >
-                                <Button 
-                                    as={Link}
-                                    to='/createEvent'
-                                    floated="right"
-                                    positive
-                                    inverted
-                                    icon="plus circle"
-                                    style={{ margin: -15 }}                                                    
-                                />
-                            </Menu.Item>
-                        </Fragment>
-                    )}
-                    {authenticated ? (
-                        <SignedInMenu
-                            auth={auth}
-                            profile={profile}
-                            signOut={this.handleSignOut}
-                            currentUser={auth.currentUser}
-                        />
-                    ) : (
-                            <SignedOutMenu
-                                signIn={this.handleSignIn}
-                                register={this.handleRegister}
-                            />
-                        )}
-                </Container>
-            </Menu>
-        );
-    }
+          {authenticated ? (
+            <SignedInMenu
+              signOut={this.handleSignOut}
+              profile={profile}
+              auth={auth}
+            />
+          ) : (
+            <SignedOutMenu
+              signIn={this.handleSignIn}
+              register={this.handleRegister}
+            />
+          )}
+        </Container>
+      </Menu>
+    );
+  }
 }
 
-export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
+export default withRouter(
+  withFirebase(
+    connect(
+      mapState,
+      actions
+    )(NavBar)
+  )
+);
